ffdf892edb717ac90e2c5ab7d19d7ad49ec87fbb
Chris Morabito <chris.morabito@woolpert.com>
2022-12-01 10:23:34 -0500

    Fix style

--- application/frontend/src/app/shared/components/place-autocomplete/place-autocomplete.component.ts
+++ application/frontend/src/app/shared/components/place-autocomplete/place-autocomplete.component.ts
@@ -37,11 +37,7 @@ import { MatFormFieldControl } from '@angular/material/form-field';
 import { Subject, Subscription } from 'rxjs';
 import { ILatLng } from 'src/app/core/models';
 import { isPlaceId, PlacesService } from 'src/app/core/services';
-import {
-  isLatLngString,
-  stringToLatLng,
-  toDispatcherLatLng,
-} from 'src/app/util';
+import { isLatLngString, stringToLatLng, toDispatcherLatLng } from 'src/app/util';
 
 /** Mimic the Angular material error state implementation to play nice with the material stepper */
 class PlaceAutocompleteComponentBase {
