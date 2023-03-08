af6f66a8b25f32d19350625b50c00e576bf7c2d5
Jason McCollum <jason.mccollum@woolpert.com>
2023-03-08 15:21:53 -0800

    Exclude breaks from timeline that occur outside of the vehicle's day

--- application/frontend/src/app/core/selectors/timeline.selectors.ts
+++ application/frontend/src/app/core/selectors/timeline.selectors.ts
@@ -112,9 +112,10 @@ const getTimeline = (
     'Visits and travel steps not aligned.'
   );
 
-  const breaks = route.breaks ? route.breaks.map(createBreakSegment) : [];
   const vehicleStartTime = route.vehicleStartTime ? durationSeconds(route.vehicleStartTime) : null;
   const vehicleEndTime = route.vehicleEndTime ? durationSeconds(route.vehicleEndTime) : null;
+  // Create break segments, exlcuding those that outside of the vehicle's day
+  const breaks = route.breaks ? route.breaks.map(createBreakSegment).filter(brk => brk.startTime.greaterThanOrEqual(vehicleStartTime) && brk.startTime.lessThanOrEqual(vehicleEndTime)) : [];
 
   // Add breaks before travel to the timeline
   for (const brk of breaks) {
