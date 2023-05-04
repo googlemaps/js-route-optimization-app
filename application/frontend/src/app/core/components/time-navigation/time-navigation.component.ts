9b4a3b9380bb356bdd6e4d5d361db352dfc53bdc
Jason McCollum <jason.mccollum@woolpert.com>
2023-05-04 15:18:07 -0700

    Add a date selector to solution view

--- application/frontend/src/app/core/components/time-navigation/time-navigation.component.ts
+++ application/frontend/src/app/core/components/time-navigation/time-navigation.component.ts
@@ -15,9 +15,12 @@ import {
   Inject,
   Input,
   LOCALE_ID,
+  OnChanges,
   Output,
+  SimpleChanges,
   ViewEncapsulation,
 } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { Range } from 'src/app/shared/models';
 
 @Component({
@@ -27,7 +30,7 @@ import { Range } from 'src/app/shared/models';
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class TimeNavigationComponent {
+export class TimeNavigationComponent implements OnChanges {
   @Input() range: Range;
   @Input() rangeOffset: number;
   @Input() nowRangeOffset: number;
@@ -36,7 +39,21 @@ export class TimeNavigationComponent {
   @Input() timezoneOffset = 0;
   @Output() rangeOffsetChange = new EventEmitter<number>();
 
+  currentOffsetDate: Date;
+
   constructor(@Inject(LOCALE_ID) private locale: string) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.rangeOffset || changes.timezoneOffset) {
+      this.currentOffsetDate = new Date((this.rangeOffset + this.timezoneOffset) * 1000);
+      this.currentOffsetDate.setTime(this.currentOffsetDate.getTime() + (this.currentOffsetDate.getTimezoneOffset() * 60 * 1000));
+    }
+  }
+
+  onDateChange(event: MatDatepickerInputEvent<Date>): void {
+    const utcSeconds = (event.value.getTime() + (event.value.getTimezoneOffset() * 60) * 1000) / 1000;
+    this.rangeOffsetChange.emit(utcSeconds + this.timezoneOffset);
+  }
 
   onNextClick(): void {
     this.rangeOffsetChange.emit(this.nextRangeOffset);
