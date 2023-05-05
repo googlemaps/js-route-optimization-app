fc4ac4e9144ba69cf8e21dd8ae0d55c225fb6217
Jason McCollum <jason.mccollum@woolpert.com>
2023-05-05 11:06:23 -0700

    Limit calendar and offset ranges to global start and end

--- application/frontend/src/app/core/components/time-navigation/time-navigation.component.ts
+++ application/frontend/src/app/core/components/time-navigation/time-navigation.component.ts
@@ -34,19 +34,36 @@ export class TimeNavigationComponent implements OnChanges {
   @Input() range: Range;
   @Input() rangeOffset: number;
   @Input() nowRangeOffset: number;
-  @Input() previousRangeOffset: number;
-  @Input() nextRangeOffset: number;
   @Input() timezoneOffset = 0;
+  @Input() globalDuration: [Long, Long];
   @Output() rangeOffsetChange = new EventEmitter<number>();
 
   currentOffsetDate: Date;
+  globalStart: Date;
+  globalEnd: Date;
+  previousRangeOffset: number;
+  nextRangeOffset: number;
 
   constructor(@Inject(LOCALE_ID) private locale: string) {}
 
   ngOnChanges(changes: SimpleChanges): void {
+    if (changes.globalDuration) {
+      this.globalStart = this.getGlobalStart();
+      this.globalEnd = this.getGlobalEnd();
+    }
+
     if (changes.rangeOffset || changes.timezoneOffset) {
       this.currentOffsetDate = new Date((this.rangeOffset + this.timezoneOffset) * 1000);
       this.currentOffsetDate.setTime(this.currentOffsetDate.getTime() + (this.currentOffsetDate.getTimezoneOffset() * 60 * 1000));
+      
+      const previousDate = new Date(this.currentOffsetDate);
+      previousDate.setDate(previousDate.getDate() - 1);
+      this.previousRangeOffset = this.globalStart.getTime() <= previousDate.getTime() ?  previousDate.getTime() / 1000 : null;
+
+      const nextDate = new Date(this.currentOffsetDate);
+      nextDate.setDate(nextDate.getDate() + 1);
+      this.nextRangeOffset = nextDate.getTime() / 1000;
+      this.nextRangeOffset = this.globalEnd.getTime() >= nextDate.getTime() ?  nextDate.getTime() / 1000 : null;
     }
   }
 
@@ -65,6 +82,18 @@ export class TimeNavigationComponent implements OnChanges {
 
   onNowClick(): void {
     this.rangeOffsetChange.emit(this.nowRangeOffset);
+  }
+
+  getGlobalStart(): Date {
+    const date = new Date((this.globalDuration[0].toNumber() + this.timezoneOffset) * 1000);
+    date.setTime(date.getTime() + (date.getTimezoneOffset() * 60 * 1000));
+    return date;
+  }
+
+  getGlobalEnd(): Date {
+    const date = new Date((this.globalDuration[1].toNumber() + this.timezoneOffset) * 1000);
+    date.setTime(date.getTime() + (date.getTimezoneOffset() * 60 * 1000));
+    return date;
   }
 
   getFormattedRangeOffset(): string {
