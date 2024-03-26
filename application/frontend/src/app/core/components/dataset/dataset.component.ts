59d6f78438dd83e20bb8ead9a5efbe90178fd5e4
Jason McCollum <jason.mccollum@woolpert.com>
2024-03-26 14:25:47 -0700

    Make scenario name input resize to fit content

--- application/frontend/src/app/core/components/dataset/dataset.component.ts
+++ application/frontend/src/app/core/components/dataset/dataset.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectScenarioName } from '../../selectors/dispatcher.selectors';
 import { saveScenarioName } from '../../actions/dispatcher.actions';
@@ -9,16 +16,24 @@ import { saveScenarioName } from '../../actions/dispatcher.actions';
   styleUrls: ['./dataset.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DatasetComponent implements OnInit {
+export class DatasetComponent implements AfterViewInit {
+  @ViewChild('scenarioInput') scenarioInput: ElementRef;
+  @ViewChild('resizeText') resizeText: ElementRef;
   scenarioName: string;
 
   constructor(private store: Store, private detectRef: ChangeDetectorRef) {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.store.select(selectScenarioName).subscribe((value) => {
       this.scenarioName = value;
+      this.updateInputSize();
       this.detectRef.markForCheck();
     });
+  }
+
+  updateInputSize(): void {
+    // this.scenarioInput.nativeElement.size = this.scenarioName.length || 1;
+    this.resizeText.nativeElement.textContent = this.scenarioName;
   }
 
   onSave(): void {
