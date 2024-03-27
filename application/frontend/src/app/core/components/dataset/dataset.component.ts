fc2f32ca83c8e85f047693c797e849bfeb005eec
Jason McCollum <jason.mccollum@woolpert.com>
2024-03-27 15:00:33 -0700

    Let use confirm and cancel with Enter and Escape

--- application/frontend/src/app/core/components/dataset/dataset.component.ts
+++ application/frontend/src/app/core/components/dataset/dataset.component.ts
@@ -21,12 +21,14 @@ export class DatasetComponent implements AfterViewInit {
   @ViewChild('resizeText') resizeText: ElementRef;
 
   scenarioName = '';
+  originalScenarioName = '';
 
   constructor(private store: Store, private detectRef: ChangeDetectorRef) {}
 
   ngAfterViewInit(): void {
     this.store.select(selectScenarioName).subscribe((value) => {
       this.scenarioName = value;
+      this.originalScenarioName = value;
       this.updateInputSize();
       this.detectRef.markForCheck();
     });
@@ -36,7 +38,22 @@ export class DatasetComponent implements AfterViewInit {
     this.resizeText.nativeElement.textContent = this.scenarioName;
   }
 
-  onSave(): void {
+  onKeyDown(event: KeyboardEvent): void {
+    if (event.key === 'Enter') {
+      this.scenarioInput.nativeElement.blur();
+      this.save();
+    } else if (event.key === 'Escape') {
+      this.reset();
+      this.updateInputSize();
+      this.scenarioInput.nativeElement.blur();
+    }
+  }
+
+  reset(): void {
+    this.scenarioName = this.originalScenarioName;
+  }
+
+  save(): void {
     this.store.dispatch(saveScenarioName({ scenarioName: this.scenarioName }));
   }
 }
