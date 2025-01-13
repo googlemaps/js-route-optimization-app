03072d9e53d05b5213a9e1900b95ac1bfbef63c9
Chris Morabito <chris.morabito@woolpert.com>
2025-01-13 11:28:10 -0500

    Upgrade Google providers

--- deployment/terraform.tf
+++ deployment/terraform.tf
@@ -12,11 +12,11 @@ terraform {
     }
     google = {
       source  = "hashicorp/google"
-      version = "4.22.0"
+      version = "6.15.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "4.22.0"
+      version = "6.15.0"
     }
   }
 }
