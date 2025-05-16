5fb89b2d38043a1ac7e5e9a0b252169822446384
Jason McCollum <jason.mccollum@woolpert.com>
2025-05-16 08:21:47 -0700

    fix: zip validation order (#238)

--- application/frontend/src/app/core/containers/upload-dialog/upload-dialog.component.ts
+++ application/frontend/src/app/core/containers/upload-dialog/upload-dialog.component.ts
@@ -163,6 +163,12 @@ export class UploadDialogComponent {
       return;
     }
 
+    this.fileInvalid = false;
+    this.zipContentsInvalid = false;
+    this.zipContentCountInvalid = false;
+    this.zipMissingScenario = false;
+    this.zipMissingSolution = false;
+
     this.selectedFilename = file.name.replace(/\.[^/.]+$/, '');
 
     this.validatingUpload = true;
@@ -331,11 +337,11 @@ export class UploadDialogComponent {
     let solution;
     files.forEach((file) => {
       if (!scenario || !Object.keys(scenario).length) {
-        scenario = this.validateScenario(file.content);
+        scenario = this.validateScenario(file.content).scenario;
       }
 
       if (!solution || !Object.keys(solution).length) {
-        solution = this.validateSolution(file.content);
+        solution = this.validateSolution(file.content).solution;
       }
     });
 
@@ -374,35 +380,32 @@ export class UploadDialogComponent {
    * Only validates the message/value structure
    */
   scenarioValidator(_: UntypedFormControl): ValidationErrors | null {
-    try {
-      const scenario = this.validateScenario(this.json);
-
-      if (this.scenario == null) {
-        this.scenario = scenario;
-      }
-    } catch (error) {
+    const res = this.validateScenario(this.json);
+    if (this.scenario == null && res.isValid) {
+      this.scenario = res.scenario;
+      return null;
+    } else {
       // eslint-disable-next-line no-console
-      console.log('Invalid request format:', error);
+      console.error(res.validationResult);
       return { requestFormat: true };
     }
-    return null;
   }
 
-  validateScenario(json: any): Scenario {
+  validateScenario(json: any): { scenario: Scenario; isValid: boolean; validationResult?: any } {
     const validationResult = this.uploadService.validateScenarioFormat(json);
     if (validationResult) {
-      throw validationResult;
+      return { scenario: null, isValid: false, validationResult };
     }
 
-    return this.dispatcherService.objectToScenario(json);
+    return { scenario: this.dispatcherService.objectToScenario(json), isValid: true };
   }
 
-  validateSolution(json: any): Solution {
+  validateSolution(json: any): { solution: Solution; isValid: boolean; validationResult?: any } {
     const validationResult = this.uploadService.validateSolutionFormat(json);
     if (validationResult) {
-      throw validationResult;
+      return { solution: null, isValid: false, validationResult };
     }
 
-    return this.dispatcherService.objectToSolution(json);
+    return { solution: this.dispatcherService.objectToSolution(json), isValid: true };
   }
 }
